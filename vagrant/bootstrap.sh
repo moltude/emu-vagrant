dbf7a62ecff81656c9591c959c5c809eb963902a
Scott Williams <scott.brandon.williams@gmail.com>
2015-03-12 22:38:45 -0400

    working deploy with vagrant. remember to use -l with su'

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -34,41 +34,38 @@ echo '**** Installing dependencies...'
 
 # taken from 
 # http://askubuntu.com/questions/146921/how-do-i-apt-get-y-dist-upgrade-without-a-grub-config-prompt
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+# sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" update
+# sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 
 # Update all modules
 # sudo apt-get update -y && sudo apt-get dist-upgrade -y
 
 # sudo apt-get update --fix-missing # > /dev/null 2>&1
 
-sudo apt-get install -y curl #> /dev/null 2>&1
+sudo apt-get install -y curl > /dev/null 2>&1
 
-sudo apt-get install make #> /dev/null 2>&1
+# sudo apt-get install -y make #> /dev/null 2>&1
 
 #Perl
-sudo apt-get install -y perl
+# sudo apt-get install -y perl
 sudo apt-get install -y xinetd > /dev/null 2>&1
 
 
 # CPAN
-sudo apt-get install cpanminus -y # > /dev/null 2>&1
+# sudo apt-get install cpanminus -y # > /dev/null 2>&1
 
 # Update outdated
-sudo cpanm App::cpanoutdated # > /dev/null 2>&1
+# sudo cpanm App::cpanoutdated # > /dev/null 2>&1
 
-echo '...finished *****'
-# Apache
-#apt-get install -y apache2
-#rm -rf /var/www
-#ln -fs /vagrant /var/www
-### Required for 64-bit ###
-# apt-get install ia32-libs
+### Required for 64-bit OS ###
+sudo apt-get install -y libpam0g:i386 > /dev/null 2>&1
+
+echo 'Finished installing dependencies *****'
 
 ############################
 ###   INSTALL TEXPRESS   ###
 ############################
-chmod 755 -R ${EMU_HOME}
+sudo chmod 755 -R ${EMU_HOME}
 
 # do as emu
 su emu <<'EOF'
@@ -84,18 +81,31 @@ echo 'Downloading texpress...' ${TEXPRESS_LINK}
 wget -q -O texpress.sh ${TEXPRESS_LINK} >> ${EMU_HOME}'/install_log/log.txt' 2>&1
 echo 'Finished.'
 
-sh texpress.sh
-. ./.profile
+sh texpress.sh >> ${EMU_HOME}'/install_log/log.txt' 2>&1
+source .profile
 export TEXGROUP=emuadmin
+
+
+####
+echo 'Current working directing...'${PWD}
+####
+
+echo '##################################'
+echo '###### INSTALLING TEXPRESS #######'
+echo '##################################'
 
 bin/texinstall ${EMU_HOME}/texpress/${TEXPRESS_VER} >> ${EMU_HOME}'/install_log/log.txt' 2>&1
 cd ${EMU_HOME}/texpress/${TEXPRESS_VER}
-. ./.profile
-texbldperms
+source .profile
+
+bin/texbldperms
 bin/texlicinfo
 
+echo 'export TEXHOME=${EMU_HOME}/texpress/${TEXPRESS_VER}/' >> ${EMU_HOME}'/.profile'
+source .profile
+
 echo ${TEXPRESS_LIC} > '.license'
-bin/texlicset < '.license'
+${EMU_HOME}/texpress/${TEXPRESS_VER}/bin/texlicset < '.license'
 
 \rm -fr install
 
@@ -104,14 +114,16 @@ ln -s ${TEXPRESS_VER} 8.3
 
 echo 'Texpress install complete.' >> ${EMU_HOME}'/install_log/log.txt' 2>&1
 
-# Add to PATH
-export PATH=$PATH:${EMU_HOME}/texpress/8.3/bin
-
 # TEXAPI INSTALL
+echo '##################################'
+echo '####### INSTALLING TEXAPI  #######'
+echo '##################################'
+
 cd ${EMU_HOME}/texpress
 mkdir ${TEXAPI_VER}
-wget -q -O texapi.sh ${TEXAPI_LINK} >>${EMU_HOME}'/install_log/log.txt' 2>&1
+wget -q -O texapi.sh ${TEXAPI_LINK} >> ${EMU_HOME}'/install_log/log.txt' 2>&1
 sh texapi.sh -i ${EMU_HOME}/texpress/${TEXAPI_VER} >> ${EMU_HOME}'/install_log/log.txt' 2>&1
+
 \rm -f texapi
 ln -s ${TEXAPI_VER} texapi
 \rm -f texapi.sh
@@ -125,40 +137,58 @@ cd ${CLIENT}/install
 # Download client server
 echo 'Downloading client EMu server...'
 wget -q -O emu-${CLIENT}.sh ${EMU_LINK} >> ${EMU_HOME}'/install_log/log.txt' 2>&1
-echo 'Begining install..'
+
+echo '#############################'
+echo '### INSTALLING EMU SERVER ###'
+echo '#############################'
+
 # Changed this from emu-clientname-YYYYMMDD.sh to a standard file name 
 sh emu-${CLIENT}.sh >> ${EMU_HOME}'/install_log/log.txt' 2>&1
 
-# Add to PATH
-export PATH=$PATH:${EMU_HOME}/emu/${CLIENT}/bin
+source .profile
 
-. ./.profile
-bin/emuinstall ${CLIENT} >> ${EMU_HOME}'/install_log/log.txt' 2>&1
+bin/emuinstall ${CLIENT} # >> ${EMU_HOME}'/install_log/log.txt' 2>&1
 cd ${EMU_HOME}/${CLIENT}
-cp  .profile.parent  ../.profile
+cp .profile.parent  ../.profile
+
+# set PATH at /home/emu
+###
+echo 'Current working directing for .profile PATH...'${PWD}
+###
+
+echo "export PATH=$PATH:${EMU_HOME}/${CLIENT}/bin:${EMU_HOME}/texpress/8.3/bin:" >> '.profile'
+source .profile
+
+# commented out to try and restore a working build
 cd ..
 
 # Add a single line client clientname to the file and save it. 
 # If a default client is already registered then you may leave the existing value.
 
-echo 'Current working directory before setting client' $(pwd)
+# 
+echo 'Current working directing...'${PWD}
+# 
+echo "client ${CLIENT}" >> '.profile-local'
+source .profile-local
 
-echo client ${CLIENT} >> '.profile-local'
+# reload the profiles 
+source .profile
 
-.  ./.profile
 client ${CLIENT}
 
-echo 'Current working directory after setting client' $(pwd)
+cd etc
 
-# cd etc
+###############################
 ### OPTIONAL
+# 
 # 17.	View the config.sample file.
 # If you wish to alter some of these settings to suit the client then:
 # 	1.	Enter cp config.sample config
 # 	2.	Enter vi config
 # 	3.	Edit as appropriate, then save the file.
 # Be careful to set EMUSMTPSERVER to the hostname of your mail server machine.
-#cd ..
+# cd ..
+###############################
 
 emubldlinks >> ${EMU_HOME}'/install_log/log.txt' 2>&1
 emulutsrebuild -t -f >> ${EMU_HOME}'/install_log/log.txt' 2>&1
@@ -174,11 +204,12 @@ echo 'Cleaning up install..'
 # settings should be examined. Please see Texpress options for a list of acceptable values. 
 
 # Enter vi .profile-local and add the Texpress options to the file. An example file is:
-echo client ${CLIENT} >> '.profile-local'
-TEXPRESSOPTS="${TEXPRESSOPTS} dateorder=mdy dateformat='dd MMM yyyy'"
-export TEXPRESSOPTS
 
-cd etc
+echo "TEXPRESSOPTS=${TEXPRESSOPTS} dateorder=mdy dateformat='dd MMM yyyy'
+export TEXPRESSOPTS" >> '.profile-local'
+source .profile-local
+
+# cd etc
 # Configure iMu Server
 cp imuserver.conf.sample imuserver.conf
 # 	2.	Enter vi imuserver.conf
@@ -193,6 +224,11 @@ cp imuserver.conf.sample imuserver.conf
 # run as root
 
 #3.	Add new services to the end of /etc/services
+
+# source /home/emu/.profile
+# source /home/emu/upmaa/.profile
+
+
 EOF
 
 echo "emu	20000/tcp 
@@ -232,6 +268,7 @@ sudo ${EMU_HOME}/${CLIENT}/bin/emuboot
 
 echo 'EMu install complete'
 
-su emu <<'EOF'
+su -l emu <<'EOF'
+source /home/emu/.profile
 emuload status
 EOF
